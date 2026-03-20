4b726041f187e641ae08c2c8ea80825ef99cf163
IvStar10 sch <--no-email-->
2026-03-20 14:03:59 +0000

    Added a TODO comment.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # TODO: Добавить поиск по словарю (что-то вроде гугл-переводчика).
 # TODO: Разобраться с tkinter.Treeview наконец-то!
+# TODO: Написать тесты, хотя бы для data.py
 import logging
 from handlers.gui import MainWindow
 from handlers.data import JSON
